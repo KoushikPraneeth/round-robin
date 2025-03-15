0688dfe7779eaef3d7ee09b54c82a08664243851
Praneeth Koushik <koushikpraneeth0@gmail.com>
2025-03-15 18:43:02 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ const app = express();
 
 // Middleware
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS, PATCH');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, x-auth-token');
   res.header('Access-Control-Allow-Credentials', 'true');
@@ -37,7 +37,7 @@ app.use((req, res, next) => {
 });
 app.options('*', cors());
 app.use(cors({
-  origin: 'http://localhost:8080',
+  origin: '*',
   credentials: true
 }));
 app.use(express.json());
